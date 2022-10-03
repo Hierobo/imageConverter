4c924a31e243181ce5988461cdf848e12922b5a9
Hierobo <109856749+Hierobo@users.noreply.github.com>
2022-10-03 17:57:59 +0200

    updatet commentary and deleted legacy code

--- converter.py
+++ converter.py
@@ -3,9 +3,9 @@ from PIL import Image
 from tkinter import filedialog
 import shutil, os, sys
 
-#appends image path to image_paths list, copies image to script directory
+#(appends image path to image_paths list), copies image to script directory, and displays it in the gui listbox
 def openFile():
-    global image_paths
+    #global image_paths
     global image_counter
     filepath = filedialog.askopenfilename() #opens file explorer to choose image
     a, b = os.path.split(filepath)
@@ -19,7 +19,11 @@ def openFile():
     #image_paths.append(fileclone)
 
 
-
+#function using the save() method of the Pillow library which determins the desired file format via string
+#image = internal image name used for convertion  e.g. image0
+#format = user specified image format to convert into
+#imageKey = original(user) image name e.g. birthday picture.jpg
+#savePath = directory to save the image into
 def convertIm(image, format, imageKey, savePath):
     a, b = os.path.splitext(imageKey)
     newImageKey = a + format
@@ -28,7 +32,7 @@ def convertIm(image, format, imageKey, savePath):
     if image != outfile:
         try:
             with Image.open(image) as im:
-                im.save(outfile)
+                im.save(outfile)           #save() automaticly detects the image type which, e.g. outfile = picture.pdf  ->  save() will convert the image into a pdf
                 image_key_value.update({newImageKey:outfile})
                 my_gui.insert_listbox(newImageKey)
                 savePath = savePath + "/" + newImageKey
@@ -37,28 +41,10 @@ def convertIm(image, format, imageKey, savePath):
         except OSError:
             print("cannot convert", image, format, imageKey)
 
-def convert_and_save(image, format, imageKey):
-    a, b = os.path.splitext(imageKey)
-    newImageKey = a + format
-    f, e = os.path.splitext(image)
-    outfile = f + format  # .jpg, .pdf   ;  save() converts file into specified format
-    if image != outfile:
-        try:
-            with Image.open(image) as im:
-                im.save(outfile)
-                savePath = filedialog.askdirectory   #ask the user for directory to save image in
-                print("test")
-                print(savePath)
-                savePath = savePath + outfile
-                print(savePath)
-                #shutil.copy2(outfile, savePath)
-
-        except OSError:
-            print("cannot convert", image)
-
-def deleteImagesOnStart():   #deletes all internal images on start
+#deletes all internal images in the script directory 
+def deleteImagesOnStart():   
     my_gui.image_listbox.delete(0, 'end')
-    directory = os.fsencode(sys.path[0])
+    directory = os.fsencode(sys.path[0])   #sys.path[0] returns script directory
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.startswith("image"):
@@ -72,6 +58,7 @@ def deleteImagesOnStart():   #deletes all internal images on start
         else:
             continue
 
+#deletes single file, currently not used
 def deleteImage(fileClone):
     try:
         os.remove(fileClone)
@@ -80,7 +67,7 @@ def deleteImage(fileClone):
     except PermissionError:
         print("You do not have permission to delete that")
 
-image_paths = []
+#image_paths = []
 image_counter = 0      #counter to help name the file clones
 
 image_key_value = {}   #dictionary to connect the original filenames with the program-internal filenames
